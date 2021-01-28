604dbf2756a1c14dbfa827d51bf589a9a299a2fe
Maryna Manzhula <maryna.manzhula@gmail.com>
2021-01-28 22:18:30 +0300

    Add implementation of LinkedList and CharactersCollection

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,23 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([100, 3, -5, 10]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('sdfsdfsd');
+const stringSorter = new Sorter(charactersCollection);
+stringSorter.sort();
+console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(5);
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+linkedList.print();
