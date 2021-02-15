90e3758416a57d869790c2c0a8389672c3841f01
Maryna Manzhula <maryna.manzhula@gmail.com>
2021-02-15 22:57:27 +0300

    Abstact Sorter class

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,16 +1,13 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([100, 3, -5, 10]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection('sdfsdfsd');
-const stringSorter = new Sorter(charactersCollection);
-stringSorter.sort();
+const stringSorter = charactersCollection.sort();
 console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
@@ -18,6 +15,5 @@ linkedList.add(500);
 linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(5);
-const linkedListSorter = new Sorter(linkedList);
-linkedListSorter.sort();
+linkedList.sort();
 linkedList.print();
